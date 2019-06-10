da9787b3c5075837eee9e5c4eedf16df1229b1d4
VINU VINU <VINU VINU@VinuVInu>
2019-06-10 20:16:53 +0530

    completed

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -36,24 +36,36 @@ public class Hostel{
 	               String line = null;
 	            
 	               //write your code here !!!
-	               while ((line = br.readLine()) != null) {
+	               while ((line = br.readLine()) != null) 
+	               {
 		               String[] splited = line.split("\\s+");
-		               String checkName = splited[0];
+		               String checkName = name;
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
+	               if(splited[0].equals(name))
+	                   {
+	            	   return true;
+	                   }
 	               }
-	               
+	                 
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try
+    	   {
+    		   writedata();
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e);
+           }
+    	
        }
-
+      
        public static boolean verifyStudent(int regNo){
          try{
              BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
@@ -75,7 +87,7 @@ public class Hostel{
        public static boolean verifyName(String name){
     	   boolean chk = true;
     	   
-    	   //write your code here
+    	chk = readData(name);
     	   
     	   return chk;
         }
